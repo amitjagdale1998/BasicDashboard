388a81899eeb50b7bc60a9c617e100d2c68f3b05
amitjagdale1998 <106459780+amitjagdale1998@users.noreply.github.com>
2023-04-03 16:41:08 +0530

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-const connectiondb=mongoose.connect('mongodb+srv://amitjagdale:Satara123@cluster0.pq65k.mongodb.net/blackcofferdb?retryWrites=true&w=majority',{
+const connectiondb=mongoose.connect('mongodb+srv://{mongodbusername}:{mongopassword}@cluster0.pq65k.mongodb.net/blackcofferdb?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>console.log("connection sucessfull")).catch((err)=>console.log(err));
